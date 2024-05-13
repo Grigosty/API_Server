a33f0ff7aaf78e6960b23a39b99c070c96dbb308
Grigosty <122815869+Grigosty@users.noreply.github.com>
2024-05-14 00:11:09 +0300

    Update startScript.sh

--- startScript.sh
+++ startScript.sh
@@ -5,6 +5,7 @@ sudo service postgresql start
 
 # Создание нового пользователя базы данных
 sudo -u postgres createuser --superuser postgres
+sudo -u postgres psql -c "ALTER USER postgres WITH ENCRYPTED PASSWORD 'password';"
 
 # Вход в интерактивный режим psql
 psql -U postgres
